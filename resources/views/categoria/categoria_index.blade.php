4fc4da08e83117a5c5b45007264b8caa39edd7e6
Jennifer.lima <128004109+JenniferLimaR@users.noreply.github.com>
2024-08-29 21:47:57 -0300

    value

--- resources/views/categoria/categoria_index.blade.php
+++ resources/views/categoria/categoria_index.blade.php
@@ -22,6 +22,59 @@
                       <th>Nome</th>
                       <th>Ações</th>
                     </tr>
+                    @extends('adminlte::page')
+
+                    @section('content')
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-md-8">
+                                <div class="card">
+                                    <div class="card-header">{{ __('Dashboard') }}</div>
+
+                                    <a href="{{ url('/categoria/create') }}" class="btn btn-success mb-3" role="button" aria-pressed="true">Criar</a>
+
+                                    @if (session('mensagem'))
+                                    <div class="alert alert-success">
+                                        {{ session('mensagem') }}
+                                    </div>
+                                    @endif
+
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                              <th>ID</th>
+                                              <th>Nome</th>
+                                              <th>Ações</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach($categorias as $value)
+                                            <tr>
+                                              <td>{{ $value->id }}</td>
+                                              <td>{{ $value->nome }}</td>
+                                              <td>
+                                                <div class="btn-group" role="group">
+                                                  <a href="{{ url('/categoria/' . $value->id) }}" class="btn btn-primary" role="button" aria-pressed="true">Visualizar</a>
+
+                                                  <a href="{{ url('/categoria/' . $value->id . '/edit') }}" class="btn btn-warning" role="button" aria-pressed="true">Editar</a>
+
+                                                  <form method="POST" action="{{ url('/categoria/' . $value->id) }}" class="d-inline">
+                                                      @method('DELETE')
+                                                      @csrf
+                                                      <input type="submit" class="btn btn-danger" value="Excluir">
+                                                  </form>
+                                                </div>
+                                              </td>
+                                            </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @endsection
 
                     @foreach($categorias as $value)
                     <tr>
